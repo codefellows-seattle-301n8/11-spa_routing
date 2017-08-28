a0c365cb16471f0134db6b5eda6304aa6ca2e043
nosrac77 <carson.newton@outlook.com>
2017-08-28 13:24:18 -0700

    finished my TODO list tasks.

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -2,9 +2,7 @@
 var app = app || {};
 
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
-page('/articles', app.articleController.getArticles);
-page('/about', app.aboutController.getAboutMe);
+
+// Carson here. Remember that the parameters necessary for the page.js routes are the URLs (look in index.html) and the callback functions associated with the URLs (look in articleController.js && aboutController.js). YOU GOT THIS.
 
 // TODO: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
-
-page();
